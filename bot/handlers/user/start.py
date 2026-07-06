072315b5c764febaba964de52048b66829bf2fe1
voin57rus <88772553+voin57rus@users.noreply.github.com>
2026-07-06 22:01:17 +0300

    Update start.py

--- bot/handlers/user/start.py
+++ bot/handlers/user/start.py
@@ -481,7 +481,10 @@ async def cmd_buy_tokens(message: Message, state: FSMContext):
         # Добавляем HTML-форматирование (в БД хранится чистый текст)
         text = text.replace('📸 После оплаты', '<b>📸 После оплаты</b>')
         text = text.replace(' By Oleg', ' <b>By Oleg</b>')
-        text = text.replace('📢 Канал поддержки: https://t.me/Answer_na_Questions')
+        text = text.replace('💰 Пополнение токенов', '<b>💰 Пополнение токенов</b>')
+        text = text.replace(
+    '📢 Канал поддержки: https://t.me/Answer_na_Questions',
+    '📢 <a href="https://t.me/Answer_na_Questions">Канал поддержки</a>')
     else:
         # Фоллбэк если нет в БД
         text = (
