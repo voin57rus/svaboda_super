8076c7ab32912cb3d3697564df657b207b6d6f8c
voin57rus <88772553+voin57rus@users.noreply.github.com>
2026-07-06 13:34:02 +0300

    Update start.py

--- bot/handlers/user/start.py
+++ bot/handlers/user/start.py
@@ -426,8 +426,8 @@ async def cmd_buy_tokens(message: Message, state: FSMContext):
         text = page_text.replace('{tariff}', tariff).replace('{tokens}', tokens)
         # Добавляем HTML-форматирование (в БД хранится чистый текст)
         text = text.replace('📸 После оплаты', '<b>📸 После оплаты</b>')
-        text = text.replace('👨‍💻 By Oleg', '👨‍💻 <b>By Oleg</b>')
-        text = text.replace('📢 Канал поддержки: https://t.me/Answer_na_Questions', '📢 <a href="https://t.me/Answer_na_Questions">Канал поддержки</a>')
+        #text = text.replace('👨‍💻 By Oleg', '👨‍💻 <b>By Oleg</b>')
+        #text = text.replace('📢 Канал поддержки: https://t.me/Answer_na_Questions', '📢 <a href="https://t.me/Answer_na_Questions">Канал поддержки</a>')
     else:
         # Фоллбэк если нет в БД
         text = (
